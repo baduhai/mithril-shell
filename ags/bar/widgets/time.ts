7798f1f118f85d8009d01f8d6638cf574c96435d
William <baduhai@proton.me>
2025-02-06 12:37:36 -0300

    vertacl time, please

--- ags/bar/widgets/time.ts
+++ ags/bar/widgets/time.ts
@@ -3,7 +3,12 @@ import { config } from "lib/settings.js";
 
 const time = Variable("", {
   // TODO: Format date in program.
-  poll: config.vertical ? [1000, 'date +"%-d %b %H:%M"'] : [1000, 'date +"%H\n%M"'],
+  // poll: config.vertical ? [1000, 'date +"%-d %b %H:%M"'] : [1000, 'date +"%H\n%M"'],
+  if (config.vertical) {
+    poll: [1000, 'date +"%H\n%M"'],
+  } else {
+    poll: [1000, 'date +"%-d %b %H:%M"'],
+}
 });
 
 export const Time = () =>
