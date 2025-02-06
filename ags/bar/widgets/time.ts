8583700d5a692bb56482de576f9617d4a8a9163d
William <baduhai@proton.me>
2025-02-06 12:46:55 -0300

    THis should work

--- ags/bar/widgets/time.ts
+++ ags/bar/widgets/time.ts
@@ -3,12 +3,7 @@ import { config } from "lib/settings.js";
 
 const time = Variable("", {
   // TODO: Format date in program.
-  // poll: config.vertical ? [1000, 'date +"%-d %b %H:%M"'] : [1000, 'date +"%H\n%M"'],
-  if (config.vertical) {
-    poll: [1000, 'date +"%H\n%M"'],
-  } else {
-    poll: [1000, 'date +"%-d %b %H:%M"'],
-}
+  poll: config.vertical ? [1000, 'date +"%H\n%M"'] : [1000, 'date +"%-d %b %H:%M"'],
 });
 
 export const Time = () =>
