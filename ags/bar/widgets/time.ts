9c0b7d2837e80c6e04204b57888595e72678f7f1
William <baduhai@proton.me>
2025-02-06 11:34:07 -0300

    Vertical layout complete!

--- ags/bar/widgets/time.ts
+++ ags/bar/widgets/time.ts
@@ -3,7 +3,7 @@ import { config } from "lib/settings.js";
 
 const time = Variable("", {
   // TODO: Format date in program.
-  poll: config.vertical ?[1000, 'date +"%H\n%M"'] : [1000, 'date +"%-d %b %H:%M"'],
+  poll: config.vertical ? [1000, 'date +"%H\n%M"'] : [1000, 'date +"%-d %b %H:%M"'],
 });
 
 export const Time = () =>
@@ -11,6 +11,7 @@ export const Time = () =>
     child: Widget.Label({
       className: "time",
       label: time.bind(),
+      justify: 2,
     }),
     onClicked: () => Utils.execAsync("swaync-client -t"),
   });
